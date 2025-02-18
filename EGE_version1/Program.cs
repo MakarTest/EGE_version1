b59331c3eb7b9310b834955c241f11ae22de1d42
Мария <mahadrum@gmail.com>
2025-02-18 12:12:10 +0300

    Добавьте файлы проекта.

---
+++ EGE_version1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGE_version1
+{
+    internal class Program
+    {
+        static void print(List<string> lst)
+        { 
+             foreach(var item in lst)
+                Console.WriteLine(item);
+            Console.WriteLine();
+        }
+
+        static void print(List<string> lst, List<List<string>> someLst)
+        {
+            for (int i = 0; i < lst.Count; i++)
+            {
+                Console.WriteLine(lst[i] + " " + someLst[i].Count);
+                for (int j = 0; j < someLst[i].Count; j++)
+                    Console.WriteLine(someLst[i][j]);
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StreamReader fileEGE = new StreamReader("request.txt");
+            List <string> subjects = new List<string>();
+            List<List<string>> listOfSubjects = new List<List<string>>();
+            
+            while(!fileEGE.EndOfStream) 
+            { 
+                string[] words = fileEGE.ReadLine().Split();
+                int ind = subjects.IndexOf(words[3]);
+                if (ind == -1)
+                {
+                    subjects.Add(words[3]);
+                    ind = subjects.Count() - 1;
+                    listOfSubjects.Add(new List<string>());
+                }
+                listOfSubjects[ind].Add($"{words[0]} {words[1]} {words[2]}");
+            }
+            fileEGE.Close();
+            print(subjects, listOfSubjects);
+        }
+    }
+}
